bfe082ad00ac4c789eab8bab4738f980abb84fc4
root <root@localhost.localdomain>
2020-07-17 10:16:50 +0530

    adding ForeignKey

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class AppModel(models.Model):
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     desc = models.TextField()
 
