87887ea42c8641ff5b0123b321c38359569844e5
root <root@localhost.localdomain>
2020-07-17 10:49:10 +0530

    Better Validation on update

--- myapp/forms.py
+++ myapp/forms.py
@@ -11,8 +11,11 @@ class AppsForm(forms.ModelForm):
         ]
 
     def cleaned_title(self, *args, **kwargs):
+        instance = self.instance
         title = self.cleaned_data.get["title"]
         qs = AppModel.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)
         if qs.exists():
             raise forms.ValidationError("This title is already in use")
         return title
